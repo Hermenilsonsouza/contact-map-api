b2273910c145afdf9998fb66be729386c891afdf
Hermenilsonsouza <your_github_email>
2024-09-10 15:11:47 -0300

    feat: add cpf validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,16 @@
 class Contact < ApplicationRecord
     has_many :addresses, dependent: :destroy
     accepts_nested_attributes_for :addresses
+    
+    validates :cpf, presence: true, uniqueness: { case_sensitive: false }
+
+    validate :valid_cpf
+
+    
+
+    def valid_cpf
+        unless CPF.valid?(cpf)
+          errors.add(:cpf, 'cpf é inválido')
+        end
+    end
 end
