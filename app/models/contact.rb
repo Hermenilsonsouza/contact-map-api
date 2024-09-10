b1bf6ee26da5e9e63e287f8e93811beeca47cdc9
Hermenilsonsouza <your_github_email>
2024-09-10 15:44:57 -0300

    feat: add cpf validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,13 +4,13 @@ class Contact < ApplicationRecord
     
     validates :cpf, presence: true, uniqueness: { case_sensitive: false }
 
-    validate :valid_cpf
+    before_validation :valid_cpf
 
     
 
     def valid_cpf
         unless CPF.valid?(cpf)
-          errors.add(:cpf, 'cpf é inválido')
+          errors.add(:cpf, "O cpf #{self.cpf} é inválido")
         end
     end
 end
